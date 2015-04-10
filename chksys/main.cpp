ad227cfbcef272f7a2dfad43ee12a800ea071020
Eric Menezes <ericmenezesn@gmail.com>
2015-04-10 14:28:41 -0300

    Isolando código do windows para não tentar compilar no linux

--- chksys/main.cpp
+++ chksys/main.cpp
@@ -8,13 +8,14 @@ int main(int argc, char **argv)
     service.setStartupType(QtServiceController::AutoStartup);
     service.setServiceFlags(chksys::Default);
 
+#ifdef Q_OS_WIN
     ServiceController *serviceHandler = new ServiceController(QString("CheckCacic").toStdWString());
-
     if (!serviceHandler->isInstalled()){
         serviceHandler->install(QString(Identificadores::ENDERECO_PATCH_CACIC + "chksys.exe").toStdWString(),
                                 QString("CheckCacic").toStdWString());
         QThread::sleep(3);
     }
     delete serviceHandler;
+#endif
     return service.exec();
 }
